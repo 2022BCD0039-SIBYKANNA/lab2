24f5c0b340a3a1d4d4ee9a64cca87df2c9176ae4
2022BCD0039-SIBYKANNA <siby22bcd39@iiitkottayam.ac.in>
2026-01-09 21:54:15 +0530

    EXP-01 Linear Regression

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ import os
 import joblib
 
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error, r2_score
 
@@ -28,7 +28,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 
-model = RandomForestRegressor(n_estimators=100, random_state=42)
+model = LinearRegression()
 model.fit(X_train, y_train)
 
 
@@ -57,6 +57,7 @@ results = {
 
 with open("output/results/results.json", "w") as f:
     json.dump(results, f, indent=4)
+
 
 
 
